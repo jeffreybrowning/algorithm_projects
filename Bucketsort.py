2bf59e76252a6c663e5749438eef8a9f6e56b3ee
Jeff Browning <jeffbrowning05@gmail.com>
2014-09-12 00:15:04 -0700

    Minor changes to Knapsack.py

--- Bucketsort.py
+++
@@ -1,9 +0,0 @@
-def bucketsort(l):  # this is also called bin sort
-    """Bucketsort has the following proceedure:
-
-        1.  Setup array of empyy buckets
-        2.  Go over original array and put every object in its
-        bucket based on its value range. 
-        3.  Sort all non-empty buckets.
-        4.  Concatenate all buckets. 
-    """
